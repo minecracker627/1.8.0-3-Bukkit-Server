d17ba84b13849882fc5e783d1884dfc05b380002
BuildTools <mc63821@gmail.com>
2015-03-16 11:57:12 -0500

    1.8.0-3-Bukkit-Server
    This contains a PreCompiled 1.8 Bukkit Server

---
+++ LinuxStart.sh
@@ -0,0 +1,3 @@
+cd '/Volumes/Gameband/1.8.0|3-Bukkit Server'
+nano '/Volumes/Gameband/1.8.0|3-Bukkit Server/server.proporties'
+java -Xmx2G -Xms2G -jar '/Volumes/Gameband/1.8.0|3-Bukkit Server/bukkit.jar'
---
+++ StartMac.command
@@ -0,0 +1,3 @@
+cd '/Volumes/Gameband/1.8.0|3-Bukkit Server'
+nano '/Volumes/Gameband/1.8.0|3-Bukkit Server/server.properties'
+java -Xmx2G -Xms2G -jar '/Volumes/Gameband/1.8.0|3-Bukkit Server/bukkit.jar'
